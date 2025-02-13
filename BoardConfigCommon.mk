7573ae0266cdbb809055ddd11e9c601968f01b02
Yumi Yukimura <me.cafebabe@gmail.com>
2025-02-13 15:04:52 +0800

    sm6150-common: Properly switch to common QTI USB init scripts
    Change-Id: I42864edf1cf554815fa24b12892afd21835d13c7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -56,7 +56,6 @@ BOARD_KERNEL_CMDLINE := \
     androidboot.console=ttyMSM0 \
     androidboot.hardware=qcom \
     androidboot.memcg=1 \
-    androidboot.usbcontroller=a600000.dwc3 \
     cgroup.memory=nokmem,nosocket \
     loop.max_part=7 \
     lpm_levels.sleep_disabled=1 \
